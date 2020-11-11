4d9a3067bff08a6d7e8b4913ae0ecc046ce80cdb
gabriel.dorneles <gabriel.dorneles@flanp.com>
2020-11-11 11:03:37 -0300

    Criação de repositóio, inserção das aulas praticadas até atual momento

---
+++ Aula03/Aula03.cs
@@ -0,0 +1,15 @@
+using System;
+
+class Aula03{
+    static void Main(){
+        int num1, num, res;
+
+        num1 = 10;
+
+        num = 2;
+
+        res = num1 * num;
+
+        Console.WriteLine("A multiplicação de " + num1 + " vezes " + num + " é igual a: " + res);
+    }
+}
