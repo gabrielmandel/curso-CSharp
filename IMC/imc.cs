87b681460b1fff4e9cbe2b3cd0e2a7efe7d9ba7b
gabriel.dorneles <gabriel.dorneles@flanp.com>
2020-11-24 09:46:32 -0300

    batata

--- IMC/imc.cs
+++ IMC/imc.cs
@@ -26,18 +26,23 @@ namespace calculadoraIMC{
         }
         static void Main(){
             // IMC == PESO(kg) / ALTURA ^ 2
-            float massa, altura, imc;            
-            Console.Write("Digite seu peso(kg): ");
-            massa = float.Parse(Console.ReadLine());
-            Console.Write("Digite sua altura(m): ");
-            altura = float.Parse(Console.ReadLine());
+            float massa, altura, imc;    
+            char continuar = 'S' ;
+            char[,] cont = new char[2,2] {{'S','s'} , {'N','n'}};  
+          do{  
+                Console.Write("Digite seu peso(kg): ");
+                massa = float.Parse(Console.ReadLine());
+                Console.Write("Digite sua altura(m): ");
+                altura = float.Parse(Console.ReadLine());
 
-            imc = massa / (altura * altura);
-        
-            string resultado = grau(imc);
-            Console.WriteLine("Seu IMC é: {0} - {1}",imc,resultado);
-        
+                imc = massa / (altura * altura);
             
-        }
+                string resultado = grau(imc);
+                Console.WriteLine("Seu IMC é: {0} - {1}",imc,resultado);
+                Console.WriteLine("Deseja Continuar? [S/N]");
+                continuar = Char.Parse(Console.ReadLine());
+            } while(continuar == 'S'  || continuar == 's');
+                Console.WriteLine("Obrigado por usar a calculadora!");
+        } 
     }
 }
